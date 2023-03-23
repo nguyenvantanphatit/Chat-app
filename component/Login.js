7de2161f459cbd2f60304dc5978a243eba105f6c
Tan Phat <nguyenvantanphat.it@gmail.com>
2023-03-23 19:21:10 +0700

    first commit

---
+++ component/Login.js
@@ -0,0 +1,43 @@
+import Head from "next/head";
+import { ChatIcon } from "@chakra-ui/icons";
+import { Box, Button, Center, Stack } from "@chakra-ui/react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../firebaseconfig";
+
+export default function Login() {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <Center h="100vh">
+        <Stack
+          align="center"
+          backgroundColor="gray.600"
+          p={16}
+          rounded="3xl"
+          spacing={12}
+          boxShadow="lg"
+        >
+          <Box
+            backgroundColor="blue.500"
+            width="fit-content"
+            p={5}
+            rounded="3xl"
+            boxShadow="md"
+          >
+            <ChatIcon w="100" h="100" color="white" />
+          </Box>
+          <Button
+            boxShadow="md"
+            onClick={() => signInWithGoogle("", { prompt: "select_account" })}
+          >
+            Sign In with Google
+          </Button>
+        </Stack>
+      </Center>
+    </>
+  );
+}
---
+++ pages/_app.js
@@ -0,0 +1,34 @@
+import { ChakraProvider, Spinner, Center } from "@chakra-ui/react";
+import Login from "../component/Login";
+// import Sidebar from '../component/Sidebar';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebaseconfig";
+
+function MyApp({ Component, pageProps }) {
+  const [user, loading, error] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <ChakraProvider>
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      </ChakraProvider>
+    );
+  }
+  if (!user) {
+    return (
+      <ChakraProvider>
+        <Login />
+      </ChakraProvider>
+    );
+  }
+
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
